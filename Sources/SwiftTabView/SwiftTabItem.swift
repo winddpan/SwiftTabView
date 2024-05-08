a487630c92063a6720a333fb6d84931d4d9b290c
winddpan <winddpan@126.com>
2024-05-08 16:43:31 +0800

    remove unnecessary code

--- Sources/SwiftTabView/SwiftTabItem.swift
+++ Sources/SwiftTabView/SwiftTabItem.swift
@@ -2,31 +2,13 @@ import SwiftUI
 
 public extension View {
     func swiftTabItem(tag: some Hashable, @ViewBuilder _ label: @escaping (_ isSelected: Bool) -> some View) -> some View {
-        id(tag)
-            .modifier(SwiftTabItemModifier(tagId: tag, label: label))
-    }
-}
-
-struct SwiftTabItemModifier<TagID: Hashable, Label: View>: ViewModifier {
-    let tagId: TagID
-    let label: (_ isSelected: Bool) -> Label
-
-    init(tagId: TagID, label: @escaping (_ isSelected: Bool) -> Label) {
-        self.tagId = tagId
-        self.label = label
-        tryAppendBuilder()
-    }
-
-    private func tryAppendBuilder() {
         if let manager = refBuilderManager {
             let builder = { isSelected in
                 AnyView(label(isSelected))
             }
-            manager.builders.append(TagItemBuilder(tag: tagId, itemBuilder: builder))
+            manager.builders.append(TagItemBuilder(tag: tag, itemBuilder: builder))
         }
-    }
 
-    func body(content: Content) -> some View {
-        content
+        return id(tag)
     }
 }
